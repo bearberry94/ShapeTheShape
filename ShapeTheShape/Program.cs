15be0dd7a97d6d72d4b7678f438dbd0299427273
mpigeau <marina.a.pigeau@gmail.com>
2020-02-05 08:54:14 -0500

    created local project

---
+++ ShapeTheShape/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShapeTheShape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
