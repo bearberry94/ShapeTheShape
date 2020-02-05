808882e8eb705d5b3a06f24f63b9d417dfb876cb
mpigeau <marina.a.pigeau@gmail.com>
2020-02-05 13:38:04 -0500

    created Point class and a function describing the class in Main

---
+++ ShapeTheShape/Point.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapeTheShape
+{
+    class Point
+    {
+        private int x, y;
+
+        public int X { set; get; }
+
+        public int Y { set; get; }
+
+        public int MutliplyXByScalar
+        {
+            set { X *= value; }
+        }
+
+        public int MultiplyYByScalar
+        {
+            set { Y *= value; }
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                try
+                {
+                    if (i == 0) { return X; }
+                    else if (i == 1) { return Y; }
+                    else { throw new Exception("Error: invalid index"); }
+                } catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return -1;
+                }
+            }
+        }
+        
+        public void DisplayCoordinates()
+        {
+            Console.WriteLine(">> Coordinates: ({0}, {1})", X, Y);
+        }
+
+        public double CalculateDistanceFromOrigin()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+        }
+
+        public void TranslatePointByVector(int a, int b)
+        {
+            X += a;
+            Y += b;
+        }
+
+        public static Point operator ++(Point point) {
+            point.X += 1;
+            point.Y += 1;
+            return point;
+        }
+
+        public static Point operator --(Point point)
+        {
+            point.X -= 1;
+            point.Y -= 1;
+            return point;
+        }
+
+        public static bool operator true(Point point)
+        {
+            if (point.X == point.Y) { return true; }
+            else { return false; }
+        }
+
+        public static bool operator false(Point point)
+        {
+            if (point.X != point.Y) { return false; }
+            else { return true; }
+        }
+
+        public static Point operator +(Point point, int scalar)
+        {
+            point.X += scalar;
+            point.Y += scalar;
+            return point;
+        }
+
+        public Point()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
--- ShapeTheShape/Program.cs
+++ ShapeTheShape/Program.cs
@@ -4,9 +4,43 @@ namespace ShapeTheShape
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public void showcasePointClass(int a, int b) {
+            Point point1 = new Point();
+            Console.WriteLine("Created Point obj with coordinates ({0}, {1})", point1.X, point1.Y);
+            Console.WriteLine("DisplayCoordinates() displays the point's coordinates.");
+            point1.DisplayCoordinates();
+
+            Point point2 = new Point(a, b);
+            Console.WriteLine("\nCreated Point obj with coordinates ({0}, {1})", point2.X, point2.Y);
+
+            Console.WriteLine("\nCalculateDistanceFromOrigin() calculates the distance from\nthe point to the origin. For our point ({0}, {1}), it's {2:.000} to 3dp", point2.X, point2.Y, point2.CalculateDistanceFromOrigin());
+
+            Console.WriteLine("\nTranslatePointByVector(a, b) moves our point along the axes.\nIf we want to move our point ({0},{1}) by (3, -10)", point2.X, point2.Y);
+            point2.TranslatePointByVector(3, -10);
+            Console.WriteLine("our new coordinates are({0},{1})", point2.X, point2.Y);
+
+            Console.WriteLine("\nPoint.MultiplyXByScalar and Point.MultiplyYByScalar multiply the\nx and y values by scalar values, e.g. 2 and 3.");
+            point2.DisplayCoordinates();
+            point2.MutliplyXByScalar = 2;
+            point2.MultiplyYByScalar = 3;
+            point2.DisplayCoordinates();
+
+            Console.WriteLine("\nPoint[0] accesses the x-value, and Point[1] accesses the y-value.\nCoordinates are: ({0},{1}). Any other index will return an error\nE.g. Point[5]: {2}", point2[0], point2[1], point2[5]);
+
+            Console.WriteLine("\nPoint++ increases both x and y by 1.");
+            point2.DisplayCoordinates();
+            point2++;
+            point2.DisplayCoordinates();
+
+            Console.WriteLine("\nPoint-- decreases both x and y by 1.");
+            point2.DisplayCoordinates();
+            point2--;
+            point2.DisplayCoordinates();
+
+            Console.WriteLine("Point + a adds a to the x and the y value of the point. Eg. adding 10 to our coordinates.");
+            point2.DisplayCoordinates();
+            point2 += 10;
+            point2.DisplayCoordinates();
         }
     }
 }
